3c7c414cf2fef3d9d007589a9c837dbf2ef97b54
atspiirs <ats.piirsalu@gmail.com>
2024-12-02 19:14:06 +0200

    http -> https

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=2cec0e13caed47fe9a230217240911&q=Sölden&aqi=no`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=2cec0e13caed47fe9a230217240911&q=Sölden&aqi=no`)
         .then(response => response.json())
         .then(response => {
             var wind_speed = Math.round((response.current.wind_kph * 0.277778 + Number.EPSILON) * 100) / 100
